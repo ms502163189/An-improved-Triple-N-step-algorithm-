2c43a471bc431bc0f8e1c77593bd24ab627f67c1
ms502163189 <502163189@qq.com>
2023-03-21 11:05:24 +0800

    an improved Triple-N-step algorithm

---
+++ capture.py
@@ -0,0 +1,279 @@
+import os
+import time
+from pathlib import Path 
+import numpy as np
+import cv2
+import pygame
+import random
+import pathlib
+
+from config import config
+from projector import Projector
+from camera import HK_Camera
+from fringes import Fringes
+from MLP_net import del_files
+
+class ImageReader():
+    def __init__(self, config):
+        self._c = config
+
+    def read(self, case, gamma1):
+        root = pathlib.Path(self._c.measure_path)
+        images = []
+        list = [7, 4, 3]
+        imgs_f1 = []
+        imgs_f2 = []
+        imgs_f3 = []
+        for ind in range(14):
+            name_h = root / f"{case:0>2d}{ind:0>2d}h.bmp"
+            print(name_h)
+            img = cv2.imread(name_h.as_posix(), 0)
+            height, width = img.shape[:2]
+            # print("height, width",height)
+            # print("width",width)
+
+            # start_row, start_col = 0, 0
+            # end_row, end_col = height, width
+            start_row, start_col = 490, 0
+            end_row, end_col = 540, width
+
+            cropped = img[start_row:end_row, start_col:end_col]  # 将图像裁剪成1920 * 50
+            # slight gamma correction to avoid outliers
+            cropped = np.power(cropped / 255., gamma1) * 255
+            cropped = cv2.blur(cropped, (7, 7))
+            if ind < 7:
+                print("f1")
+                imgs_f1.append(cropped)
+            elif ind < 11:
+                print("f2")
+                imgs_f2.append(cropped)
+            elif ind < 14:
+                print("f3")
+                imgs_f3.append(cropped)
+        images.append(imgs_f1)
+        images.append(imgs_f2)
+        images.append(imgs_f3)
+        print("len(images[0])", len(images[0]))
+        print("len(images[1])", len(images[1]))
+        print("len(images[2])", len(images[2]))
+
+        name = f"train_data{case:03d}"
+        path = os.path.join(self._c.net_dir, name)
+        np.savez(path, image1=images[0], image2=images[1], image3=images[2])
+        return case
+
+class Capture():
+    def __init__(self, config):
+        self._c = config
+        self.prj = Projector(config)
+        self.hkc = HK_Camera()
+        self.hkc.start()
+
+    def capture_one(self, path, hv="h"):
+            i = 0
+            prj_flag = True
+            while prj_flag:
+                prj_flag = self.prj.update(hv)
+                # print("prj_flag", prj_flag)
+                time.sleep(32/96)
+                self.prj.black()
+                self.hkc.capture_one(name=path + f"{i:0>2d}{hv}.bmp")
+                i+=1
+    #标定时候用这个函数，投影图需要制作
+    def calibra_capture(self):
+        count = 15
+        root = Path(self._c.calibra_path)
+        root.mkdir(parents=True, exist_ok=True)
+        while self.prj.wait_to_begin():
+            self.capture_one(str(root)+ f"/{count:0>2d}_", hv="h")
+            self.capture_one(str(root)+ f"/{count:0>2d}_", hv="v")
+            count +=1
+
+    #测量实验时候用这个函数，12步相移的投影图需要通过fringes.py生成
+    def measure_capture(self):
+        count = 0
+        root = Path(self._c.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        while self.prj.wait_to_begin():
+            self.capture_one(str(root)+ f"/{count:0>2d}", hv="h")
+            count +=1
+
+    def measure_capture_train_data(self, num, data_num):  #生成训练时候的数据集
+        count = 0
+        flag = 0
+        root = Path(self._c.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        while self.prj.wait_to_begin():   #按空格就拍
+            for i in range(num):           #按一次空格拍100组
+                # print("pattern_path",self._c.pattern_path)
+                # print("steps",self._c.steps)
+                # print("cfg.measure_path ",self._c.measure_path )
+                self._c.gamma = random.uniform(0.8,2.0)
+                print("self._c.gamma ", self._c.gamma)
+                self._c.hv="h"
+                Fringes(self._c).save_images()
+                self._c.hv="v"
+                Fringes(self._c).save_images()
+                self.capture_one(str(root)+ f"/{count:0>2d}", hv="h")
+                case = ImageReader(self._c).read(case=count, gamma1=1.25)  # gamma1是对图像进行略微的gamma矫正
+                print("{} 组已经保存".format(case))
+                count +=1
+                if count % 10 == 0:
+                    del_files(root)
+                if count == data_num:
+                    flag = 1
+                    break
+            if flag == 1:
+                break
+        return count
+
+
+    def exit(self):
+        self.prj.exit()
+        self.hkc.exit()
+
+    def exit1(self):
+        self.hkc.exit()
+def wait_to_begin():
+    pygame.init()
+    wait_proj = True
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    # self.screen = pygame.display.set_mode((0,0))
+
+    screen.fill((0, 0, 255))
+    bg = pygame.image.load('./data/cover.png').convert()
+    blank = pygame.image.load('./data/blank.png').convert()
+    clock = pygame.time.Clock()  # 设置时钟
+    clock.tick(24)
+
+    screen.blit(bg,(0,0))
+    pygame.display.flip()
+    pygame.display.update()
+    while True:
+        clock.tick(24)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                wait_proj = False
+                exit()
+
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_SPACE]:
+            print("Begin to project patterns...")
+            break
+
+        if keys_pressed[pygame.K_ESCAPE]:
+            wait_proj = False
+            print("Projection finished.\n")
+            break
+    return wait_proj
+
+
+if __name__ == '__main__':
+    from config import config
+
+    cfg = config()
+    # cap.calibra_capture() # 标定的时候用这个
+    # input = input("生成训练数据集还是测试数据集：")
+    # if (input == "test"):
+    count = 0
+    while wait_to_begin():
+
+        cfg.steps = [9, 9, 9]
+        cfg.pattern_path = "./data/patterns/999-step/"
+        cfg.measure_path = "./data/recordings/2/9-step/"
+        # cfg.measure_path = "./data/hdr_images/"   # 如果是hdr实验，文件夹要换成这个
+        # projector = Projector(cfg)
+        if not os.path.exists(cfg.measure_path):
+            os.makedirs(cfg.measure_path)
+        # print("cfg.measure_path  ",cfg.measure_path )
+        cap = Capture(cfg)
+        root = Path(cfg.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        print("root", root)
+        cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        cap.exit1()
+        #
+        cfg.steps = [20, 20, 20]
+        cfg.pattern_path = "./data/patterns/202020-step/"
+        cfg.measure_path = "./data/recordings/2/20-step/"
+        # projector = Projector(cfg)
+        if not os.path.exists(cfg.measure_path):
+            os.makedirs(cfg.measure_path)
+        # print("cfg.measure_path  ",cfg.measure_path )
+        cap = Capture(cfg)
+        root = Path(cfg.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        print("root", root)
+        cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        cap.exit1()
+
+        cfg.steps = [12, 12, 12]
+        cfg.pattern_path = "./data/patterns/121212-step/"
+        cfg.measure_path = "./data/recordings/2/12-step/"
+        # projector = Projector(cfg)
+        if not os.path.exists(cfg.measure_path):
+            os.makedirs(cfg.measure_path)
+        # print("cfg.measure_path  ",cfg.measure_path )
+        cap = Capture(cfg)
+        root = Path(cfg.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        print("root", root)
+        cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        cap.exit1()
+        #
+        # cfg.steps = [7, 7, 7]
+        # cfg.pattern_path = "./data/patterns/777-step/"
+        # cfg.measure_path = "./data/recordings/2/7-step/"
+        # # projector = Projector(cfg)
+        # if not os.path.exists(cfg.measure_path):
+        #     os.makedirs(cfg.measure_path)
+        # cap = Capture(cfg)
+        # root = Path(cfg.measure_path)
+        # root.mkdir(parents=True, exist_ok=True)
+        # print("root", root)
+        # cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        # cap.exit1()
+        # cfg.steps = [4, 4, 4]
+        # cfg.pattern_path = "./data/patterns/444-step/"
+        # cfg.measure_path = "./data/recordings/2/4-step/"
+        # # projector = Projector(cfg)
+        # if not os.path.exists(cfg.measure_path):
+        #     os.makedirs(cfg.measure_path)
+        # cap = Capture(cfg)
+        # root = Path(cfg.measure_path)
+        # root.mkdir(parents=True, exist_ok=True)
+        # print("root", root)
+        # cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        # cap.exit1()
+        cfg.steps = [3, 3, 3]
+        cfg.pattern_path = "./data/patterns/333-step/"
+        cfg.measure_path = "./data/recordings/2/3-step/"
+
+        # projector = Projector(cfg)
+        if not os.path.exists(cfg.measure_path):
+            os.makedirs(cfg.measure_path)
+        cap = Capture(cfg)
+        root = Path(cfg.measure_path)
+        root.mkdir(parents=True, exist_ok=True)
+        print("root", root)
+        cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        cap.exit1()
+        # cfg.steps = [7, 4, 3]
+        # cfg.pattern_path = "./data/patterns/743-step/"
+        # cfg.measure_path = "./data/recordings/2/7-4-3-step/"
+        # # projector = Projector(cfg)
+        # if not os.path.exists(cfg.measure_path):
+        #     os.makedirs(cfg.measure_path)
+        # cap = Capture(cfg)
+        # root = Path(cfg.measure_path)
+        # root.mkdir(parents=True, exist_ok=True)
+        # print("root", root)
+        # cap.capture_one(str(root) + f"/{count:0>2d}", hv="h")  # 20-step
+        # cap.exit1()
+        count += 1
+    cap.exit()
+
+
+
+
+
---
+++ recons3d_copy.py
@@ -0,0 +1,260 @@
+import numpy as np
+import cv2
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from phase1 import *
+from calibration import Calibrator
+from config import config
+
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+end_header
+'''
+
+def phase_average(phi1, phi2, phi3):
+    phi_r1 = (phi1+phi2+phi3)/3
+    phi_r2 = phi_r1+2*np.pi/3
+    phi_r3 = phi_r1-2*np.pi/3
+
+    err1 = np.minimum(np.mod(phi_r1 - phi2, 2*np.pi), np.mod(phi2-phi_r1, 2*np.pi))
+    err2 = np.minimum(np.mod(phi_r2 - phi2, 2*np.pi), np.mod(phi2-phi_r2, 2*np.pi))
+    err3 = np.minimum(np.mod(phi_r3 - phi2, 2*np.pi), np.mod(phi2-phi_r3, 2*np.pi))
+
+    phi = phi_r1
+    mask2 = (err2<err1) * (err2<err3)
+    phi[mask2] = phi_r2[mask2]
+    mask3 = (err3<err1) * (err3<err2)
+    phi[mask3] = phi_r3[mask3]
+    return phi
+
+
+def unwrapped_phase(cfg, psi):
+    phi1, T1 = PE(cfg).phase_unwarping2(psi[0],psi[1],cfg.Tc[0],cfg.Tc[1])
+    phi1 = phi1*T1/cfg.Tc[0]
+    phi1 = phi1.astype(np.float32)
+    return phi1,T1
+
+def old_Triple_N_step(i,cfg,fringe_image):
+    img1_group0 = []
+    img1_group1 = []
+    img1_group2 = []
+    if i == 1:
+        name = 0
+    if i == 2:
+        name = 1
+    print(name)
+    img1_0, img1_3, img1_6 = fringe_image[name][11], fringe_image[name][3], fringe_image[name][7]
+    img1_group0.append(img1_0)
+    img1_group0.append(img1_3)
+    img1_group0.append(img1_6)
+    img1_1, img1_4, img1_7 = fringe_image[name][0], fringe_image[name][4], fringe_image[name][8]
+    img1_group1.append(img1_1)
+    img1_group1.append(img1_4)
+    img1_group1.append(img1_7)
+    img1_2, img1_5, img1_8 = fringe_image[name][1], fringe_image[name][5], fringe_image[name][9]
+    img1_group2.append(img1_2)
+    img1_group2.append(img1_5)
+    img1_group2.append(img1_8)
+
+    phase_measures0 = PE(cfg).psi_extract(images=img1_group0)  # 用origin求包裹相位
+    phase_measures1 = PE(cfg).psi_extract(images=img1_group1)  # 用2pai/3N求包裹相位
+    phase_measures2 = PE(cfg).psi_extract(images=img1_group2)  # 用4pai/3N求包裹相位
+
+    return phase_measures0,phase_measures1,phase_measures2,img1_group0,img1_group1,img1_group2
+
+def new_Triple_N_step(i,cfg,fringe_image):
+    img1_group0 = []
+    img1_group1 = []
+    img1_group2 = []
+    if i == 1:
+        name = 0
+    if i == 2:
+        name = 1
+    print(name)
+    img1_0, img1_3, img1_6 = fringe_image[name][8], fringe_image[name][2], fringe_image[name][5]
+    img1_group0.append(img1_0)
+    img1_group0.append(img1_3)
+    img1_group0.append(img1_6)
+    img1_1, img1_4, img1_7 = fringe_image[name][0], fringe_image[name][3], fringe_image[name][6]
+    img1_group1.append(img1_1)
+    img1_group1.append(img1_4)
+    img1_group1.append(img1_7)
+    img1_2, img1_5, img1_8 = fringe_image[name][1], fringe_image[name][4], fringe_image[name][7]
+    img1_group2.append(img1_2)
+    img1_group2.append(img1_5)
+    img1_group2.append(img1_8)
+
+    phase_measures0 = PE(cfg).psi_extract(images=img1_group0)  # 用origin求包裹相位
+    phase_measures1 = PE(cfg).psi_extract(images=img1_group1)  # 用2pai/3N求包裹相位
+    phase_measures2 = PE(cfg).psi_extract(images=img1_group2)  # 用4pai/3N求包裹相位
+
+    return phase_measures0,phase_measures1,phase_measures2,img1_group0,img1_group1,img1_group2
+
+class Recons3D():
+    def __init__(self, cfg):
+        self._c = cfg
+        self.calibrator = Calibrator(self._c)
+        self.map_c1, self.map_c2, self.phase_rectified, self.Q = self.calibrator.load()
+
+        self.phase_rectified = self.phase_rectified / self._c.Tc[0]
+
+        self.pe = eval(self._c.pe_method)(self._c)
+
+        self.shape = self.phase_rectified.shape
+
+    def measure(self, images, gray,aa=0):
+        if aa == 0:
+            print("wrapped phase")
+            phase = self.pe.psi_extract(images[0])
+            # phase, _ = self.pe.phase_extract(images)
+            return phase
+
+        if aa == 1:
+            print("unwrapped phase")
+            phase, _ = self.pe.basic_extract2(images)
+
+            phase1 = cv2.remap(phase, self.map_c1, self.map_c2, cv2.INTER_LINEAR)
+
+            gray = cv2.remap(gray, self.map_c1, self.map_c2, cv2.INTER_CUBIC)
+            gray_mask = gray < 35
+            # print("gray_mask", gray_mask)
+
+            s_x, s_y = np.meshgrid(np.arange(self.shape[1]), np.arange(self.shape[0]))
+            s_x, s_y = s_x.astype(np.float32), s_y.astype(np.float32)
+            x = np.zeros_like(self.phase_rectified).astype(np.float32)
+
+            alpha = 0.9
+            for i in range(10):
+                alpha *= 0.9
+                wp = cv2.remap(self.phase_rectified, s_x - x, s_y, cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
+                diff = wp - phase1
+                if i < 5:
+                    diff = cv2.blur(diff, (3, 3))
+                x = x + alpha * diff.astype(np.float32)
+            disp = x
+            mask = disp < -600
+            disp[mask] = np.nan
+            mask = disp > 700
+            disp[mask] = np.nan
+            # disp[gray_mask] = np.nan
+
+            points = cv2.reprojectImageTo3D(disp.astype(np.float32), self.Q)
+                # self.points = self.points[45:-35, 40:-5,:]
+
+            if self._c.debug:
+                plt.figure(figsize=(16, 6))
+                plt.subplot(1, 2, 1)
+                plt.imshow(phase1)
+                plt.colorbar()
+                plt.title("phase measured")
+                plt.subplot(1, 2, 2)
+                plt.imshow(self.phase_rectified)
+                plt.colorbar()
+                plt.title("phase rectified")
+
+                plt.figure(figsize=(16, 6))
+                plt.imshow(disp)
+                plt.colorbar()
+                plt.title("disparity")
+            return phase, points
+        if aa == 2:
+            old_psi = []
+            p_m0, p_m1, p_m2, image_pai_2N_f1, image_oringe_f1, image_pai_N_f1 = old_Triple_N_step(
+                1, self._c, images)
+            correct_old_triple_n_l = phase_average(p_m0, p_m1, p_m2)
+            phase_measures33, phase_measures44, phase_measures55, image_pai_2N_f2, image_oringe_f2, image_pai_N_f2 = old_Triple_N_step(
+                2, self._c, images)
+            correct_old_triple_n_h = phase_average(phase_measures33, phase_measures44, phase_measures55)
+            old_psi.append(correct_old_triple_n_l)
+            old_psi.append(correct_old_triple_n_h)
+
+            old_Triple_N_step_unwrapped_phase, old_Triple_N_step_T = unwrapped_phase(self._c, old_psi)
+
+            phase1 = cv2.remap(old_Triple_N_step_unwrapped_phase, self.map_c1, self.map_c2, cv2.INTER_LINEAR)
+
+            gray = cv2.remap(gray, self.map_c1, self.map_c2, cv2.INTER_CUBIC)
+            gray_mask = gray < 35
+            # print("gray_mask", gray_mask)
+
+            s_x, s_y = np.meshgrid(np.arange(self.shape[1]), np.arange(self.shape[0]))
+            s_x, s_y = s_x.astype(np.float32), s_y.astype(np.float32)
+            x = np.zeros_like(self.phase_rectified).astype(np.float32)
+
+            alpha = 0.9
+            for i in range(10):
+                alpha *= 0.9
+                wp = cv2.remap(self.phase_rectified, s_x - x, s_y, cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
+                diff = wp - phase1
+                if i < 5:
+                    diff = cv2.blur(diff, (3, 3))
+                x = x + alpha * diff.astype(np.float32)
+            disp = x
+            mask = disp < -600
+            disp[mask] = np.nan
+            mask = disp > 700
+            disp[mask] = np.nan
+
+            points = cv2.reprojectImageTo3D(disp.astype(np.float32), self.Q)
+            return p_m0, p_m1, p_m2,correct_old_triple_n_l, old_Triple_N_step_unwrapped_phase, points
+        if aa == 3:
+            new_psi = []
+            # 新的triple-N方法
+            p_m0, p_m1, p_m2, image_4pai_3N_f1, image_oringe_f1, image_2pai_3N_f1 = new_Triple_N_step(
+                1, self._c, images)
+            correct_new_triple_n_l = phase_average(p_m0, p_m1, p_m2)
+            phase_measures66, phase_measures77, phase_measures88, image_4pai_3N_f2, image_oringe_f2, image_2pai_3N_f2 = new_Triple_N_step(
+                2, self._c, images)
+            correct_new_triple_n_h = phase_average(phase_measures66, phase_measures77, phase_measures88)
+            new_psi.append(correct_new_triple_n_l)
+            new_psi.append(correct_new_triple_n_h)
+            new_triple_n_step_unwrapped_phase, new_triple_n_step_t = unwrapped_phase(self._c, new_psi)
+
+            phase1 = cv2.remap(new_triple_n_step_unwrapped_phase, self.map_c1, self.map_c2, cv2.INTER_LINEAR)
+
+            gray = cv2.remap(gray, self.map_c1, self.map_c2, cv2.INTER_CUBIC)
+            gray_mask = gray < 35
+            # print("gray_mask", gray_mask)
+
+            s_x, s_y = np.meshgrid(np.arange(self.shape[1]), np.arange(self.shape[0]))
+            s_x, s_y = s_x.astype(np.float32), s_y.astype(np.float32)
+            x = np.zeros_like(self.phase_rectified).astype(np.float32)
+
+            alpha = 0.9
+            for i in range(10):
+                alpha *= 0.9
+                wp = cv2.remap(self.phase_rectified, s_x - x, s_y, cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
+                diff = wp - phase1
+                if i < 5:
+                    diff = cv2.blur(diff, (3, 3))
+                x = x + alpha * diff.astype(np.float32)
+            disp = x
+            mask = disp < -600
+            disp[mask] = np.nan
+            mask = disp > 700
+            disp[mask] = np.nan
+
+            points = cv2.reprojectImageTo3D(disp.astype(np.float32), self.Q)
+
+            return p_m0, p_m1, p_m2,correct_new_triple_n_l, new_triple_n_step_unwrapped_phase, points
+
+    def remap(self, img):
+        return cv2.remap(img, self.map_c1, self.map_c2, cv2.INTER_CUBIC)
+
+    def save_points(self, fn, points=None):
+        # if points is not None
+        # verts = points
+        # else: 
+        verts = self.points if points is None else points
+        # verts = self.points.reshape(-1, 3)
+        verts = verts.reshape(-1, 3)
+        mask = ~np.isnan(np.sum(verts, axis=-1))
+        verts = verts[mask]
+        # verts[np.isnan(verts)] = 0.00
+        with open(fn, 'wb') as f:
+            f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+            np.savetxt(f, verts, fmt='%f %f %f')
