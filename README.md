55a9e46b1ff6c903331aca19e975e7fd54f3e98b
qqq <2225106769@qq.com>
2023-03-21 14:31:18 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# An-improved-Triple-N-step-algorithm-
+# An-improved-Triple-N-step-algorithm
+# 文件说明 
+
+ - data文件夹
+ 该文件夹内是所有的实验数据
+ ./data/calibration/2/  保存了标定后的文件
+ ./data/patterns/         保存了生成的条纹图
+ ./data/recordings/2/  保存了照相机拍摄的被测物体条纹图
+ ./data/results/             保存了Triple-N-steps-real.ipynb实际测试的结果图
+ 
+ - calibration.py
+ 标定投影仪和照相机使用，标定的结果会保存在./data/calibrations/2/calibration_result.npz文件内
+ 
+ - camera.py
+照相机设置文件，主要在capture.py中使用
+ 
+ - capture.py
+ 照相机拍照时使用，投影仪投射20步、12步、9步、3步相移条纹投影在物体，照相机拍摄图片。拍摄的图片分别保存在./data/recordings/2/20-step、./.../12-step、./.../9-step、./.../3-step内中
+ 
+ - config.py
+ 配置文件，里面包含条纹的基本参数：频率、相移步数、保存文件等等
+ 
+ - fringes.py
+ 生成条纹，在这里主要是分别生成20、12、9、3步相移条纹
+ - phase.py
+主要用来解相位，可以得到包裹相位以及解包裹相位。
+ - projector.py
+投影仪的设置文件，在capture.py中调用
+ - recons3d.py
+用于实际测试的3维重建，完成相位到点的变换
+ - Triple-N steps.ipynb
+仿真测试提出的Triple-N steps相移算法的效果
+ - Triple-N-steps-real.ipynb
+实际测试提出的Triple-N steps相移算法的效果，结果还会保存在./data/results/内
+
+# 实验流程：
+# *标定过程：*
+ - 首先用三频四步来进行标定，修改config.py里面的配置
+ 
+ - 接着运行fringes.py生成相应的电子版条纹图，包括一张背景
+ 
+ - 接着打开capture.py，下图所示，标定和实验要用不同的函数，记得改注释
+
+ - 相机捕获到图片后，一组棋盘格会生成13张图片，12张条纹图，还有一个棋盘格背景图用于检测角点
+ 
+ - 之后运行calibration.py即可，其标定结果会保存在./data/2/calibration_result.npz
+ 
+ - 最后会存储极线矫正和畸变矫正的一些参数，深度差异矩阵（三维重建），相位预处理矩阵
+ # *实验过程：*
+
+ - 首先修改config.py里面的配置，均在两频下完成，修改频率值
+
+ - 接着运行fringes.py生成相应的20步、12步、9步、3步电子版条纹图及背景图
+
+ - 接着打开capture.py，标定和实验要用不同的函数，记得改注释（文件内已经用注释标记清楚了）
+ 
+ - 相机捕获到图片后，分别保存在./data/recordings/2/20-step、./.../12-step、./.../9-step、./.../3-step内中
+
+ - 之后运行Triple-N-steps-real.ipynb即可查看实际测试的运行效果，可以对比看到我们改进的Triple-N-steps方法与传统的Triple-N-steps以及9步相移法、三步相移法解相位及三维重建的精度。
+
+ - 运行Triple-N-steps.ipynb可以查看理论仿真运行结果，仿真运行效果不需要上述过程，直接运行即可看到效果。
+
+
--- capture.py
+++ capture.py
@@ -12,55 +12,6 @@ from projector import Projector
 from camera import HK_Camera
 from fringes import Fringes
 
-class ImageReader():
-    def __init__(self, config):
-        self._c = config
-
-    def read(self, case, gamma1):
-        root = pathlib.Path(self._c.measure_path)
-        images = []
-        list = [7, 4, 3]
-        imgs_f1 = []
-        imgs_f2 = []
-        imgs_f3 = []
-        for ind in range(14):
-            name_h = root / f"{case:0>2d}{ind:0>2d}h.bmp"
-            print(name_h)
-            img = cv2.imread(name_h.as_posix(), 0)
-            height, width = img.shape[:2]
-            # print("height, width",height)
-            # print("width",width)
-
-            # start_row, start_col = 0, 0
-            # end_row, end_col = height, width
-            start_row, start_col = 490, 0
-            end_row, end_col = 540, width
-
-            cropped = img[start_row:end_row, start_col:end_col]  # 将图像裁剪成1920 * 50
-            # slight gamma correction to avoid outliers
-            cropped = np.power(cropped / 255., gamma1) * 255
-            cropped = cv2.blur(cropped, (7, 7))
-            if ind < 7:
-                print("f1")
-                imgs_f1.append(cropped)
-            elif ind < 11:
-                print("f2")
-                imgs_f2.append(cropped)
-            elif ind < 14:
-                print("f3")
-                imgs_f3.append(cropped)
-        images.append(imgs_f1)
-        images.append(imgs_f2)
-        images.append(imgs_f3)
-        print("len(images[0])", len(images[0]))
-        print("len(images[1])", len(images[1]))
-        print("len(images[2])", len(images[2]))
-
-        name = f"train_data{case:03d}"
-        path = os.path.join(self._c.net_dir, name)
-        np.savez(path, image1=images[0], image2=images[1], image3=images[2])
-        return case
-
 class Capture():
     def __init__(self, config):
         self._c = config
@@ -96,36 +47,6 @@ class Capture():
         while self.prj.wait_to_begin():
             self.capture_one(str(root)+ f"/{count:0>2d}", hv="h")
             count +=1
-
-    def measure_capture_train_data(self, num, data_num):  #生成训练时候的数据集
-        count = 0
-        flag = 0
-        root = Path(self._c.measure_path)
-        root.mkdir(parents=True, exist_ok=True)
-        while self.prj.wait_to_begin():   #按空格就拍
-            for i in range(num):           #按一次空格拍100组
-                # print("pattern_path",self._c.pattern_path)
-                # print("steps",self._c.steps)
-                # print("cfg.measure_path ",self._c.measure_path )
-                self._c.gamma = random.uniform(0.8,2.0)
-                print("self._c.gamma ", self._c.gamma)
-                self._c.hv="h"
-                Fringes(self._c).save_images()
-                self._c.hv="v"
-                Fringes(self._c).save_images()
-                self.capture_one(str(root)+ f"/{count:0>2d}", hv="h")
-                case = ImageReader(self._c).read(case=count, gamma1=1.25)  # gamma1是对图像进行略微的gamma矫正
-                print("{} 组已经保存".format(case))
-                count +=1
-                if count % 10 == 0:
-                    del_files(root)
-                if count == data_num:
-                    flag = 1
-                    break
-            if flag == 1:
-                break
-        return count
-
 
     def exit(self):
         self.prj.exit()
@@ -166,20 +87,8 @@ def wait_to_begin():
             break
     return wait_proj
 
-def dir_is_or_not_exist(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-if __name__ == '__main__':
-    from config import config
-
-    cfg = config()
-    # cap.calibra_capture() # 标定的时候用这个
-    # input = input("生成训练数据集还是测试数据集：")
-    # if (input == "test"):
-    count = 0
+def measure_capture(cfg,count):
     while wait_to_begin():
-
         cfg.steps = [9, 9, 9]
         cfg.pattern_path = "./data/patterns/999-step/"
         cfg.measure_path = "./data/recordings/2/9-step/"
@@ -226,6 +135,23 @@ if __name__ == '__main__':
         cap.exit1()
         count += 1
     cap.exit()
+
+def dir_is_or_not_exist(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+if __name__ == '__main__':
+    from config import config
+
+    cfg = config()
+    # # 标定的时候用下面三行
+    # cap = Capture(cfg)
+    # cap.calibra_capture()
+    # cap.exit()
+
+    # 测试的时候用下面两行
+    count = 0
+    measure_capture(cfg,count)
 
 
 
